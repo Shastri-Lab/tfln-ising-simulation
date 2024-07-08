8bbec0604e2cd2da1896b856eedbd8cd5cc4fb5b
hughmor <hugh.morison@hotmail.ca>
2024-07-08 13:37:01 -0400

    new sequences and .mat generation file

---
+++ folding-lattice-proteins/generate_mat_files.py
@@ -0,0 +1,55 @@
+from hp_lattice import *
+from math import ceil, floor
+from dimod.utilities import qubo_to_ising
+from dwave.samplers import SimulatedAnnealingSampler
+from dwave_to_isingmachine import (
+    get_idx_vector,
+    flatten_spin_matrix,
+    vector_to_spin_matrix,
+    J_dict_to_mat,
+    h_dict_to_mat,
+    save_qubo_model_to_ising_mat,
+)
+
+def setup_hp_isingmachine(seq, overwrite=False):
+    sequence = seq['sequence']
+    seq_len = len(sequence)
+    lattice_size_x, lattice_size_y = seq.get('latdim', (10,10))
+    lambdas = seq.get('lambdas', (2.1, 2.4, 3.0))
+    target_energy = seq['min_energy']
+    mat_filename = f'mat_files/{seq["name"]}_{lattice_size_y}x{lattice_size_x}.mat'
+    
+    try:
+        with open(mat_filename, 'r') as f:
+            pass
+        if not overwrite:
+            print(f"MATLAB file {mat_filename} already exists. Skipping...")
+            return
+    except FileNotFoundError:
+        pass
+    
+    model = Lattice_HP_QUBO(
+        dim = [lattice_size_y, lattice_size_x],
+        sequence=sequence,
+        Lambda=lambdas,
+    )
+    Q = model.interaction_matrix()
+    h_dict, J_dict, offset_ising = qubo_to_ising(Q)
+
+    idx_vector = get_idx_vector(seq_len, lattice_size_y, lattice_size_x)
+    h = h_dict_to_mat(h_dict, idx_vector)
+    J = J_dict_to_mat(J_dict, idx_vector)
+    
+    print(f"Saving Ising model to MATLAB file {mat_filename}...", end="")
+    save_qubo_model_to_ising_mat(model, mat_filename, target_energy=target_energy)
+    print("done.")
+
+    return h, J, model, target_energy
+
+if __name__ == '__main__':
+    import json
+    with open('protein_sequences.json', 'r') as f:
+        hp_sequences = json.load(f)
+
+    for seq in hp_sequences:
+        setup_hp_isingmachine(seq, overwrite=False)
